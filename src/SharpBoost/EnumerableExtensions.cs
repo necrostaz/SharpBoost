7cd3ee59de24a2eeb422c7d65e7b9c254c59345f
Stanislav Kuzmin <necrostaz@gmail.com>
2014-04-14 12:04:08 +0300

    ForEach extension

---
+++ src/SharpBoost/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharpBoost {
+    public static class EnumerableExtensions {
+        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action) {
+            // ReSharper disable PossibleMultipleEnumeration
+            items.ArgumentNullCheck("items");
+            action.ArgumentNullCheck("action");
+
+            foreach (var item in items)
+                action(item);
+            // ReSharper restore PossibleMultipleEnumeration
+        }
+
+
+    }
+}
