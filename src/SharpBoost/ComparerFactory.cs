fcad4e8e13d56135cb04d76d6c7287bcf878ac63
Stanislav Kuzmin <necrostaz@gmail.com>
2014-04-17 14:02:42 +0300

    added ComparerFactory and DictionaryExtensions

---
+++ src/SharpBoost/ComparerFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharpBoost {
+    public static class ComparerFactory {
+        public static IEqualityComparer<T> CreateEqualityComparer<T>(Func<T, T, bool> equals, Func<T, int> hash = null) {
+            return new EqualityComparer<T>(equals, hash);
+        }
+
+        public static IComparer<T> CreateComparer<T>(Func<T, T, int> compare) {
+            return new Comparer<T>(compare);
+        }
+    }
+
+    class Comparer<T> : IComparer<T> {
+        private readonly Func<T, T, int> _compare;
+        public Comparer(Func<T, T, int> compare) {
+            _compare = compare.ArgumentNullCheck("_compare");
+        }
+
+
+        #region IComparer<T> Members
+
+        int IComparer<T>.Compare(T x, T y) {
+            return _compare(x, y);
+        }
+
+        #endregion
+    }
+
+    class EqualityComparer<T> : IEqualityComparer<T> {
+        private readonly Func<T, T, bool> _equals;
+        private readonly Func<T, int> _hash;
+
+        public EqualityComparer(Func<T, T, bool> equals, Func<T, int> hash = null) {
+            _equals = equals.ArgumentNullCheck("equals");
+            _hash = hash;
+        }
+
+        #region IEqualityComparer<T> Members
+
+        bool IEqualityComparer<T>.Equals(T x, T y) {
+            return _equals(x, y);
+        }
+
+        int IEqualityComparer<T>.GetHashCode(T obj) {
+            return _hash.Return(f => f(obj), obj.GetHashCode());
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ src/SharpBoost/DictionaryExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharpBoost {
+    public static class DictionaryExtensions {
+        public static OrdDictionary<TKey, TValue, TEqualityComparer> ToOrdDictionary<TKey, TValue, TEqualityComparer>(
+            this IDictionary<TKey, TValue> dictionary, TEqualityComparer comparer)
+        where TEqualityComparer : IEqualityComparer<TKey> {
+            return new OrdDictionary<TKey, TValue, TEqualityComparer>(dictionary.ArgumentNullCheck("dictionary"), comparer);
+        }
+
+        public static OrdDictionary<TKey, TValue, IEqualityComparer<TKey>> ToOrdDictionary<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary,
+            Func<TKey, TKey, bool> equals, Func<TKey, int> hash = null) {
+            return dictionary.ToOrdDictionary(ComparerFactory.CreateEqualityComparer(equals, hash));
+        }
+
+
+        public static OrdDictionary<string, TValue, IEqualityComparer<string>> ToCaseInsensitiveKeysDictionary<TValue>(
+            this IDictionary<string, TValue> dictionary) {
+            return dictionary.ToOrdDictionary((IEqualityComparer<string>)StringComparer.InvariantCultureIgnoreCase);
+        }
+
+    }
+
+    public sealed class OrdDictionary<TKey, TValue, TEqualityComparer> : Dictionary<TKey, TValue> where TEqualityComparer : IEqualityComparer<TKey> {
+        public OrdDictionary(TEqualityComparer comparer) : base(comparer) { }
+        public OrdDictionary(int capacity, TEqualityComparer comparer) : base(capacity, comparer) { }
+        public OrdDictionary(IDictionary<TKey, TValue> dictionary, TEqualityComparer comparer) : base(dictionary, comparer) { }
+    }
+}
