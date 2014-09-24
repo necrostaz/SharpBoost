33611ebb3ff2968db8c6d93d9abec6103a8ce26a
Stanislav Kuzmin <necrostaz@gmail.com>
2014-09-24 13:49:15 +0300

    added CastAs extension

--- src/SharpBoost/ObjectExtensions.cs
+++ src/SharpBoost/ObjectExtensions.cs
@@ -1,7 +1,12 @@
-﻿namespace SharpBoost {
+﻿
+namespace SharpBoost {
     public static class ObjectExtensions {
         public static T CastTo<T>(this object o) {
             return (T) o;
+        }
+
+        public static T CastAs<T>(this object o) where T : class{
+            return o as T;
         }
     }
 }
